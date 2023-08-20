9011fe6ff892e4e34a98568af5f9024e9d13fa29
Alexandio <al.espinosaa@duocuc.cl>
2023-08-20 00:53:31 -0400

    v0.2
    ahora esta en un js lo que se muestra,
    Pendiente de hacer que la font se haga chica

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  Productos: any = [];
 
+  constructor(
+    private router: Router,
+    private http: HttpClient) { }
+
+
+  ngOnInit() {
+    this.getProductos().subscribe(res => {
+      console.log("Res", res)
+      this.Productos = res;
+    })
+  }
+
+  getProductos() {
+    return this.http
+      .get("assets/datos_internos/productos.json")
+      .pipe(
+        map((res: any) => {
+          return res.data;
+        })
+      )
+  }
 }
