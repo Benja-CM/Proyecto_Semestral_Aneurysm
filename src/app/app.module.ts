a659fd7a57dfc981aaa673cff7c1282cf3df160d
fernanman1207 <101334456+fernanman1207@users.noreply.github.com>
2023-08-25 17:52:24 -0400

    no encontre nada... solo una pagina de registro para arreglar
    otra vez no puedo cumplir con lo que me piden y traigo algo diferente...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, IonApp, } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 
 @NgModule({
 
