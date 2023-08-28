a1c64e660220b6ec4de32f68357d6173f9c51fba
fernanman1207 <101334456+fernanman1207@users.noreply.github.com>
2023-08-28 14:41:18 -0400

    los venditos validators

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  ionicForm!: FormGroup;
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.ionicForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+        ],
+      ],
+      mobile: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+    });
   }
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+  submitForm = () => {
+    if (this.ionicForm.valid) {
+      console.log(this.ionicForm.value);
+      return false;
+    } else {
+      return console.log('Please provide all the required values!');
+    }
+  };
 
 }
